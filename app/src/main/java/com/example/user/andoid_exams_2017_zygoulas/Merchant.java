44d8bed2fd05e48954aad4ab4a322c3b9a495891
Zygnik <nikoszygoulas@gmail.com>
2017-01-27 11:24:49 +0200

    second

---
+++ app/src/main/java/com/example/user/andoid_exams_2017_zygoulas/Merchant.java
@@ -0,0 +1,63 @@
+package com.example.user.andoid_exams_2017_zygoulas;
+
+/**
+ * Created by user on 27/1/2017.
+ */
+
+public class Merchant {
+
+    private String id;
+    private String legalName;
+    private String category;
+    private String address;
+    private String imageUrl;
+    private String review;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLegalName() {
+        return legalName;
+    }
+
+    public void setLegalName(String legalName) {
+        this.legalName = legalName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+}
